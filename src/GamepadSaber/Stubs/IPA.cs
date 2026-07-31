135d34a2ac66f60fc75c5ec68b30e98f1dc93c39
Kyzmiz <149882676+Reihone@users.noreply.github.com>
2026-07-31 21:17:26 +0300

    Add IPA namespace with plugin attributes and logger

--- src/GamepadSaber/Stubs/IPA.cs
+++ src/GamepadSaber/Stubs/IPA.cs
@@ -1,1 +1,32 @@
+using System;
 
+namespace IPA
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PluginAttribute : Attribute
+    {
+        public PluginAttribute(RuntimeOptions options) { }
+    }
+
+    public enum RuntimeOptions
+    {
+        SingleStartInit = 0
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class OnEnableAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class OnDisableAttribute : Attribute { }
+
+    namespace Logging
+    {
+        public class Logger
+        {
+            public void Debug(string s) { }
+            public void Info(string s) { }
+            public void Warn(string s) { }
+            public void Error(string s) { }
+        }
+    }
+}
